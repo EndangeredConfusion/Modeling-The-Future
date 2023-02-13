d241451a303be36c5af375bc3f35c8ba24b4eeea
battery-staple <56455596+battery-staple@users.noreply.github.com>
2023-02-13 12:27:31 -0500

    Fixed Kaeshev's stuff

--- parsed data.R
+++ parsed data.R
@@ -11,11 +11,11 @@ library(lattice)
 # close(con)
 # df = data.frame(csv)
 
-df = read_csv("C:\\Users\\Kaeshev_Alapati\\Downloads\\Crimes_-_2001_to_Present.csv")
-df_short = df %>% select(Date, "Community Area")
-df_short = df_short %>% mutate(Date = sub('.{12}$', '', df_short$Date))
-df_short = df_short %>% mutate(Date = as.Date(Date, format = "%m/%d/%Y"))
-df_short = df_short[df_short$Date > "2020-1-1", ]
+df <- read_csv("data/Crimes_-_2001_to_Present.csv")
+df_short <- df %>% select(Date, "Community Area")
+df_short <- df_short %>% mutate(Date = sub('.{12}$', '', df_short$Date))
+df_short <- df_short %>% mutate(Date = as.Date(Date, format = "%m/%d/%Y"))
+df_short <- df_short[df_short$Date > "2020-1-1", ]
 
 table(df_short)
 head(df_short)
