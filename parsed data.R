ef4172a77d40b73d63fc58f0d135612f637564ea
EndangeredConfusion <73362536+EndangeredConfusion@users.noreply.github.com>
2023-02-13 12:16:51 -0500

    idk added some graphs and stuff stuff

--- parsed data.R
+++ parsed data.R
@@ -1,5 +1,8 @@
 library(sqldf)
 library(tidyverse)
+library(tigerstats)
+library(lattice)
+
 #crime_data <- read.csv.sql("C:\\Users\\Kaeshev_Alapati\\Downloads\\Crimes_-_2001_to_Present.csv",
 #                           sql = "select * from file where `Date` > 2021", eol = "\n")
 # 
@@ -15,3 +18,21 @@ df_short = df_short %>% mutate(Date = as.Date(Date, format = "%m/%d/%Y"))
 df_short = df_short[df_short$Date > "2020-1-1", ]
 
 table(df_short)
+head(df_short)
+summary(df_short)
+
+
+ggplot(df_short, aes(x = Date)) +
+  geom_histogram()
+
+
+histogram(~date|`Community Area`, data=df_short,
+          type="density",
+          xlab="Date",
+          main="Crimes Over Time by Community Area")
+          # breaks=seq(from=60,to=190,by=10),
+          # layout=c(1,2))
+
+
+ggplot(df_short, aes(x = Date, y=`Community Area`)) +
+  geom_point()
